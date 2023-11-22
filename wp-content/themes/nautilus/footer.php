5a070a29903a3cc59695125c0a74495ed996a8b4
UTILISATEUR <utilisateur@pop-os.localdomain>
2023-11-22 15:44:25 +0100

    footer agenda

--- wp-content/themes/nautilus/footer.php
+++ wp-content/themes/nautilus/footer.php
@@ -1,4 +1,43 @@
         <footer class="bg-secondary text-white p-3">
+            <div class="footer-container p-5">
+                <div class="d-flex flex-row ms-5">
+                    <div class="infos col-md-4">
+                        <div>
+                            <h2>Le Nautilus</h2>
+                            <address>20 rue Jules Vernes<br>66000 Perpignan</address>
+                        </div>
+                        <div class="social-media">
+                            <a href="#"><i class="bi bi-facebook"></i></a>
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                            <a href="#"><i class="bi bi-linkedin"></i></a>
+                            <a href="#"><i class="bi bi-pinterest"></i></a>
+                        </div>
+                    </div>
+                    <div class="coworking col-md-4">
+                        <div>
+                            <h2>Espaces de coworking</h2>
+                        </div>
+                        <ul class="list-unstyled mt-3">
+                            <li><a href="#">La Salle des Machines</a></li>
+                            <li><a href="#">Le Poste de Navigation</a></li>
+                            <li><a href="#">Le SAS</a></li>
+                            <li><a href="#">L'Atelier</a></li>
+                        </ul>
+
+                    </div>
+                    <div class="about col-md-4">
+                        <div>
+                            <h2>À propos</h2>
+                        </div>
+                        <ul class="list-unstyled mt-3">
+                            <li><a href="http://nautilus.lndo.site/index.php/conditions-dutilisation/">Conditions d'utilisation</a></li>
+                            <li><a href="http://nautilus.lndo.site/index.php/privacy-policy/">Politique de confidentialité</a></li>
+                            <li><a href="http://nautilus.lndo.site/index.php/nous-contacter/">Nous contacter</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
         </footer>
     </body>
 </html>
--- wp-content/themes/nautilus/index.php
+++ wp-content/themes/nautilus/index.php
@@ -14,6 +14,11 @@
             <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/images/le-nautilus-accueil-illustration.jpg' ?>" alt='Nautilus'>
         </div>
     </section>
+    <section id="agenda" class="py-5 d-flex justify-content-center">
+        <div class="agenda-container">
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23009688&ctz=Europe%2FParis&showCalendars=0&showTabs=0&showPrint=0&mode=AGENDA&showDate=0&showNav=0&showTitle=0&src=YmNlNmNlYzE1MTcwZDc3ODIyZTM5ZjNmNTU0YWUzZTlkMzNlM2ZkMmFkYzFkZDhhNjZmNGU3MDQ5MDU3ZTQ3Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZnIuZnJlbmNoI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23F6BF26&color=%23AD1457" style="border-width:0" width="900" height="600" frameborder="0" scrolling="no"></iframe>
+        </div>
+    </section>
     <section id="service" class="py-5">
         <div class="container">
             <div class="section-heading text-center">
@@ -53,13 +58,13 @@
             </div>
         </div>
     </section>
-    <section id="service" class="py-5">
+    <section id="newsletter" class="py-5">
         <div class="container d-flex flex-row">
             <div class="row section-heading text-center" style="padding: 80px;">
                 <h2>Restez informé avec la newsletter du Nautilus</h2>
                 <p>Description</p>
             </div>
-            <div class="row section-content" style="width: 850px;">
+            <div class="row section-content mx-5" style="width: 850px;">
                 <form>
                     <div class="mb-3">
                         <label for="firstname" class="form-label">Prénom</label>
